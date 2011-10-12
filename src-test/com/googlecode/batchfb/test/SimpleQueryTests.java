6b3352524dc6ec58bc25b0fa4916ed23d2a73733
lhoriman <lhoriman@7eb0f46f-0d54-5784-3801-9043a8ae81e1>
2011-10-12 00:54:31 +0000

    Added some simple queries that print their output so we can manually validate against real results.
    git-svn-id: https://batchfb.googlecode.com/svn/trunk@122 7eb0f46f-0d54-5784-3801-9043a8ae81e1

---
+++ src-test/com/googlecode/batchfb/test/SimpleQueryTests.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2010 Jeff Schnitzer.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.googlecode.batchfb.test;
+
+import org.codehaus.jackson.node.ArrayNode;
+import org.testng.annotations.Test;
+
+import com.googlecode.batchfb.Later;
+
+/**
+ * Testing out simple queries.
+ * 
+ * @author Jeff Schnitzer
+ */
+public class SimpleQueryTests extends TestBase {
+
+	/**
+	 */
+	@Test
+	public void basicQuery() throws Exception {
+		Later<ArrayNode> nodes = this.authBatcher.query("SELECT aid FROM album WHERE owner = me()");
+		System.out.println("You have " + nodes.get().size() + " albums");
+	}
+	
+	/**
+	 * This query takes a while to run
+	 */
+	@Test
+	public void biggerQuery() throws Exception {
+//		Later<ArrayNode> nodes = this.authBatcher.query("SELECT aid, owner, cover_pid, created, name, description, size, type FROM album WHERE owner IN (SELECT uid2 FROM friend WHERE uid1 = me()) OR owner = me()");
+//		System.out.println("You and your friends have " + nodes.get().size() + " albums");
+	}
+}
