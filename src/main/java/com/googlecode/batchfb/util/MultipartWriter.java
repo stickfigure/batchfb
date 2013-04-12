103a941b3c678407b2b7d8f3ad66b0d60bed32a4
lhoriman@gmail.com <lhoriman@gmail.com@7eb0f46f-0d54-5784-3801-9043a8ae81e1>
2013-04-12 05:37:30 +0000

    Always close the writer.
    git-svn-id: https://batchfb.googlecode.com/svn/trunk@148 7eb0f46f-0d54-5784-3801-9043a8ae81e1

--- src/main/java/com/googlecode/batchfb/util/MultipartWriter.java
+++ src/main/java/com/googlecode/batchfb/util/MultipartWriter.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2010 Jeff Schnitzer.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,58 +30,60 @@ import com.googlecode.batchfb.util.RequestBuilder.BinaryAttachment;
 
 /**
  * <p>Tool which writes multipart/form-data to a stream.</p>
- * 
+ *
  * <p>See <a href="http://www.w3.org/TR/html401/interact/forms.html#h-17.13.4">http://www.w3.org/TR/html401/interact/forms.html#h-17.13.4</a>.</p>
- * 
+ *
  * @author Jeff Schnitzer
  */
 public class MultipartWriter {
-	
+
 	/** */
 	private static final String MULTIPART_BOUNDARY = "**** an awful string which should never exist naturally ****" + Math.random();
 	private static final String MULTIPART_BOUNDARY_SEPARATOR = "--" + MULTIPART_BOUNDARY;
 	private static final String MULTIPART_BOUNDARY_END = MULTIPART_BOUNDARY_SEPARATOR + "--";
-	
+
 	/** */
 	OutputStream out;
-	
+
 	/** */
 	public MultipartWriter(RequestDefinition executor) throws IOException {
 		executor.setHeader("Content-Type", "multipart/form-data; boundary=" + MULTIPART_BOUNDARY);
 		this.out = executor.getContentOutputStream();
 	}
-	
+
 	/**
 	 * Write the params as multipart/form-data.  Params can include BinaryAttachemnt objects.
 	 */
 	public void write(Map<String, Object> params) throws IOException {
-		@SuppressWarnings("resource")
 		LineWriter writer = new LineWriter(this.out);
-		
-		for (Map.Entry<String, Object> param: params.entrySet()) {
-			writer.println(MULTIPART_BOUNDARY_SEPARATOR);
-			
-			if (param.getValue() instanceof BinaryAttachment) {
-				BinaryAttachment ba = (BinaryAttachment)param.getValue();
-				writer.println("Content-Disposition: form-data; name=\"" + StringUtils.urlEncode(param.getKey()) + "\"; filename=\""
-						+ StringUtils.urlEncode(ba.filename) + "\"");
-				writer.println("Content-Type: " + ba.contentType);
-				writer.println("Content-Transfer-Encoding: binary");
-				writer.println();
-				writer.flush();
-				// Now output the binary part to the raw stream
-				int read;
-				byte[] chunk = new byte[8192];
-				while ((read = ba.data.read(chunk)) > 0)
-					this.out.write(chunk, 0, read);
-			} else {
-				writer.println("Content-Disposition: form-data; name=\"" + StringUtils.urlEncode(param.getKey()) + "\"");
-				writer.println();
-				writer.println(StringUtils.urlEncode(param.getValue().toString()));
+		try {
+			for (Map.Entry<String, Object> param: params.entrySet()) {
+				writer.println(MULTIPART_BOUNDARY_SEPARATOR);
+
+				if (param.getValue() instanceof BinaryAttachment) {
+					BinaryAttachment ba = (BinaryAttachment)param.getValue();
+					writer.println("Content-Disposition: form-data; name=\"" + StringUtils.urlEncode(param.getKey()) + "\"; filename=\""
+							+ StringUtils.urlEncode(ba.filename) + "\"");
+					writer.println("Content-Type: " + ba.contentType);
+					writer.println("Content-Transfer-Encoding: binary");
+					writer.println();
+					writer.flush();
+					// Now output the binary part to the raw stream
+					int read;
+					byte[] chunk = new byte[8192];
+					while ((read = ba.data.read(chunk)) > 0)
+						this.out.write(chunk, 0, read);
+				} else {
+					writer.println("Content-Disposition: form-data; name=\"" + StringUtils.urlEncode(param.getKey()) + "\"");
+					writer.println();
+					writer.println(StringUtils.urlEncode(param.getValue().toString()));
+				}
 			}
+
+			writer.println(MULTIPART_BOUNDARY_END);
+			writer.flush();
+		} finally {
+			writer.close();
 		}
-		
-		writer.println(MULTIPART_BOUNDARY_END);
-		writer.flush();
 	}
 }
